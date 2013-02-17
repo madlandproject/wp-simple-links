7837a8e6b75f6aabf76494e022769c21444052cb
Robin Lambell <robin@madlandproject.com>
2013-02-17 19:05:30 +0100

    added copy of plugin, changed readme

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ wp-simple-links
 ===============
 
 A simple links replacement for the feature deprecated in WP 3.5
+
+Based on the plugin : http://wordpress.org/extend/plugins/simple-links/ by Mat Lipe (http://lipeimagination.info/)
+- Code has been cleaned up. 
+- Fixed some performance issues
+- Changed images
---
+++ classes/init.php
@@ -0,0 +1,28 @@
+<?php 
+
+					/**
+					 * Brings in the necessary Classes
+					 * @since 8/15/12
+					 * @author Mat Lipe <mat@lipeimagination.info>
+					 */
+					
+$simple_links_classes = array( 
+						'wp-simple-links-post-type-tax',
+						'wp-simple-links.class'
+		                
+							);
+
+foreach( $simple_links_classes as $class ){
+	require( $class . '.php' );
+	
+}
+
+$simple_links_func = new simple_links();
+
+
+
+
+if( is_admin() ){
+	require( 'wp-simple-links.admin.class.php' );
+	$simple_links_admin_func = new simple_links_admin();
+}
